5cf84a51f99ad943628975d27dd6da5aca19360e
Samir KHERRAZ <samir.kherraz@newgeneration-sr.com>
2020-11-20 20:24:45 +0100

    fix

--- conf/var/www/api/index.php
+++ conf/var/www/api/index.php
@@ -28,33 +28,31 @@ Exemple
 
 $access_token = "{ACCESS_TOKEN}";
 if(isset($_GET['access_token']) && $_GET['access_token'] == $access_token){
-
-  $MESSAGE = "⚠️ : " . $_POST["title"];
-	$MESSAGE .= "\n";
-	$MESSAGE .= $_POST["evalMatches"]["value"];
-	$MESSAGE .= "\n";
-	$MESSAGE .= $_POST["message"];
-	    
-  $URL = "https://{SYNAPSE_SERVER}/_matrix/client/r0/rooms/{ROOM_ID}:{SYNAPSE_SERVER}/send/m.room.message?access_token={BOT_TOKEN}";
-	
-echo $MESSAGE;
-
-	$data = array(
-		'msgtype'=>'m.text',
-		'body'=>$MESSAGE
-	);
-	
-	$post_data = json_encode($data);
-	$crl = curl_init($URL);
-	curl_setopt($crl, CURLOPT_RETURNTRANFERT, true);
-	curl_setopt($crl, CURLINFO_HEADER_OUT, true);
-	curl_setopt($crl, CURLOPT_POST, true);
-	curl_setopt($crl, CURLOPT_POSTFIELDS, $post_data);
-	curl_setopt($crl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
-	
-	echo curl_exec($crl);
-	curl_close($crl);
+  $_POST = json_decode(file_get_contents('php://input'));
+  if(isset($_POST["title"])){
+    $MESSAGE = "⚠️ : " . $_POST["title"];
+    $MESSAGE .= "\n";
+    $MESSAGE .= $_POST["evalMatches"]["value"];
+    $MESSAGE .= "\n";
+    $MESSAGE .= $_POST["message"];
+    $URL = "https://{SYNAPSE_SERVER}/_matrix/client/r0/rooms/{ROOM_ID}:{SYNAPSE_SERVER}/send/m.room.message?access_token={BOT_TOKEN}";
+    
+    $data = array(
+      'msgtype'=>'m.text',
+      'body'=>$MESSAGE
+    );
+    
+    $post_data = json_encode($data);
+    $crl = curl_init($URL);
+    curl_setopt($crl, CURLOPT_RETURNTRANFERT, true);
+    curl_setopt($crl, CURLINFO_HEADER_OUT, true);
+    curl_setopt($crl, CURLOPT_POST, true);
+    curl_setopt($crl, CURLOPT_POSTFIELDS, $post_data);
+    curl_setopt($crl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));	
+    echo curl_exec($crl);
+    curl_close($crl);
+  }
 }else{
-	echo "Only post requests are allowed";
+	echo "WRONG ACCESS_TOKEN";
 }
 ?>
