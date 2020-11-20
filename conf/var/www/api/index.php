79f97168873c13f58569ad79757ee54c7c5fafa6
Samir KHERRAZ <samir.kherraz@newgeneration-sr.com>
2020-11-20 18:49:20 +0100

    exemple dataset

--- conf/var/www/api/index.php
+++ conf/var/www/api/index.php
@@ -1,4 +1,30 @@
 <?php
+/*
+Exemple
+{
+  "dashboardId":1,
+  "evalMatches":[
+    {
+      "value":1,
+      "metric":"Count",
+      "tags":{}
+    }
+  ],
+  "imageUrl":"https://grafana.com/assets/img/blog/mixed_styles.png",
+  "message":"Notification Message",
+  "orgId":1,
+  "panelId":2,
+  "ruleId":1,
+  "ruleName":"Panel Title alert",
+  "ruleUrl":"http://localhost:3000/d/hZ7BuVbWz/test-dashboard?fullscreen\u0026edit\u0026tab=alert\u0026panelId=2\u0026orgId=1",
+  "state":"alerting",
+  "tags":{
+    "tag name":"tag value"
+  },
+  "title":"[Alerting] Panel Title alert"
+}
+
+*/
 
 $access_token = "{ACCESS_TOKEN}";
 if($_POST['access_token'] == $access_token){
