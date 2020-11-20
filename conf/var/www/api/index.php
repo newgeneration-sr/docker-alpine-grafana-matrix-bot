e0cfe80fcf9899288a0488a4beb4033882dc8f2c
Samir KHERRAZ <samir.kherraz@newgeneration-sr.com>
2020-11-20 18:07:38 +0100

    fix errors

--- conf/var/www/api/index.php
+++ conf/var/www/api/index.php
@@ -1,3 +1,26 @@
-<?php 
-phpinfo();
+<?php
+
+$access_token = "{ACCESS_TOKEN}";
+if($_POST['access_token'] == $access_token){
+	$MESSAGE = "TEST !";
+	$URL = "https://{SYNAPSE_SERVER}/_matrix/client/r0/rooms/{ROOM_ID}:{SYNAPSE_SERVER}/send/m.room.message?access_token={BOT_TOKEN}";
+	
+	$data = array(
+		'msgtype'=>'m.text',
+		'body'=>$MESSAGE
+	);
+	
+	$post_data = json_encode($data);
+	$crl = curl_init($URL);
+	curl_setopt($crl, CURLOPT_RETURNTRANFERT, true);
+	curl_setopt($crl, CURLINFO_HEADER_OUT, true);
+	curl_setopt($crl, CURLOPT_POST, true);
+	curl_setopt($crl, CURLOPT_POSTFIELDS, $post_data);
+	curl_setopt($crl, CURLOPT_HTTPHEADER, array('Content-Type: application/json'));
+	
+	echo curl_exec($crl);
+	curl_close($crl);
+}else{
+	echo "Only post requests are allowed";
+}
 ?>
